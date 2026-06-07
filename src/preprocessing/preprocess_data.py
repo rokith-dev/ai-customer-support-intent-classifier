d36cd4e2a94323709153dfb6e3b08df3a453b41d
rokith-dev <rokith466@gmail.com>
2026-06-07 23:43:17 +0530

    Build preprocessing pipeline

---
+++ src/preprocessing/preprocess_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from text_cleaner import clean_text
+
+
+df = pd.read_csv("data/raw/customer_support_tickets_200k.csv")
+
+df = df[["issue_description", "category"]]
+
+df = df.dropna()
+
+df = df.drop_duplicates()
+
+df["issue_description"] = df["issue_description"].apply(clean_text)
+
+df.to_csv(
+    "data/processed/cleaned_tickets.csv",
+    index=False
+)
+
+print("Preprocessing completed successfully!")
